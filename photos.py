1c8ee9446f90b4d17694f07d746f5c8ebb965763
Ruslan-hst <gutofodi37@gmail.com>
2026-07-15 23:41:47 +0300

    Create photos.py

---
+++ photos.py
@@ -0,0 +1,137 @@
+"""
+photos.py - file_id фото моделей клюшек из закрытого TG канала
+Обновлено: 15.07.2026
+"""
+
+PHOTOS = {
+    "Bauer_Flylite_Limited_Edition_стандартный_1": "AgACAgIAAyEFAATowAsdAANValeOkFw4QiiYoP67x0mtx3kEsn0AAuAYaxtceLlK34lwDlswlNMBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_Limited_Edition_стандартный_2": "AgACAgIAAyEFAATowAsdAANWaleOkP67_M_OEOr4rNCvtPcjVrEAAuEYaxtceLlK3-SoHQ8KaQMBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_Limited_Edition_стандартный_3": "AgACAgIAAyEFAATowAsdAANXaleOkDr4D8eF76bqHXodtJ00AYcAAuIYaxtceLlKCFaP2ccFvxsBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_белый_1": "AgACAgIAAyEFAATowAsdAANPaleOjyFHMhvsYU1UnTJ4muIXDQMAAtoYaxtceLlKEh-VOcuHUpwBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_белый_2": "AgACAgIAAyEFAATowAsdAANQaleOj1nmv3d3L9Kej98npr2CktcAAtsYaxtceLlKMIpMV9-P6PwBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_белый_3": "AgACAgIAAyEFAATowAsdAANRaleOjxLB1GnXBBQRyLqoplmP-hkAAtwYaxtceLlKVHeprInqlp0BAAMCAAN5AAM9BA",
+    "Bauer_Flylite_красный_1": "AgACAgIAAyEFAATowAsdAANSaleOj-NAvGURocrPr3YXRsy3mQwAAt0YaxtceLlKNvq8wj3uy6EBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_красный_2": "AgACAgIAAyEFAATowAsdAANTaleOj0Bj7IK57FjACVFlP5qzi40AAt4YaxtceLlKuvcPPIlemG4BAAMCAAN5AAM9BA",
+    "Bauer_Flylite_красный_3": "AgACAgIAAyEFAATowAsdAANUaleOj2lr_q3dA42kUIyRhZJrrWEAAt8YaxtceLlKfEiEHNeGt4MBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_синий_1": "AgACAgIAAyEFAATowAsdAANHaldvhPy_Ilm1WvuiqrrF4nz2a0sAAiMYaxtceLlKfh8EXKwXeGcBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_синий_2": "AgACAgIAAyEFAATowAsdAANNaleOjiHXiEUZm3Xgwk3b05EMJJsAAtgYaxtceLlKSkRPw-MBIBcBAAMCAAN5AAM9BA",
+    "Bauer_Flylite_синий_3": "AgACAgIAAyEFAATowAsdAANOaleOj0r3XroU2f1w28g3P7JbA9UAAtkYaxtceLlKcBHS86XEPEsBAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_белый_1": "AgACAgIAAyEFAATowAsdAANAaldu8dEVtNPs8RMi_o-2x1aE9-cAAhAYaxtceLlKgNK_sATcUW4BAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_белый_2": "AgACAgIAAyEFAATowAsdAANBaldu92QvBJBXXCIn8Mi-aiRCq7oAAhEYaxtceLlK4X5oSPE_WesBAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_белый_3": "AgACAgIAAyEFAATowAsdAANCaldu_YK8-0wtjOCSvThlydLugB0AAhIYaxtceLlKlr740e80kf8BAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_красный_1": "AgACAgIAAyEFAATowAsdAAMhaldssupg4fqdS_2yvc8gu82V6WQAAuwXaxtceLlKPLBnTcIr9SgBAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_красный_2": "AgACAgIAAyEFAATowAsdAAMialdsuH9x9KDOl1WYDnBMwmVvLw4AAu4XaxtceLlKFSr2qKY5H_8BAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_красный_3": "AgACAgIAAyEFAATowAsdAANDaldvIjkvJ1xSuO1jhuuWKxJe6LgAAhUYaxtceLlK9LL8ZYTY728BAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_синий_1": "AgACAgIAAyEFAATowAsdAAMgaldsqzbVhMNDnF5wpXqtRErOlNEAAusXaxtceLlKHB15Eeugw_0BAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_черный_1": "AgACAgIAAyEFAATowAsdAAMealdsZUNim093Xa9KZTVly0ENE8gAAukXaxtceLlKgK4jVhC6btIBAAMCAAN5AAM9BA",
+    "Bauer_Proto_2_черный_2": "AgACAgIAAyEFAATowAsdAAMfaldsaxYVs74bKeT7nEzHR4KItBAAAuoXaxtceLlKvm5JJS4qm3IBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_белый_1": "AgACAgIAAyEFAATowAsdAANkalePclogjjgpYdcznsV9uL6D1GcAAvEYaxtceLlKBWsJUw5uDHcBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_белый_2": "AgACAgIAAyEFAATowAsdAANlalePdxzdQut0UGiX9Oq3OJi7d2cAAvIYaxtceLlK1re45yxEOU8BAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_белый_3": "AgACAgIAAyEFAATowAsdAANmalePehIOPB5JB85QTi-z0r1QN2wAAvMYaxtceLlKyozZZ-ogu4IBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_желтый_1": "AgACAgIAAyEFAATowAsdAANhalePULxe-xtqSvvW_YCDX9BmEY4AAu0YaxtceLlK-Jj5XoA4OEsBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_желтый_2": "AgACAgIAAyEFAATowAsdAANialePVZqZe5JAN84ZJr0hy9aZj10AAu8YaxtceLlKuYsJzIVzzuABAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_желтый_3": "AgACAgIAAyEFAATowAsdAANjalePaN_H6CFEfgABQzoZv6m7xXh7AALwGGsbXHi5St6klNmjJETXAQADAgADeQADPQQ",
+    "Bauer_Pulse_Graffiti_фиолетовый_1": "AgACAgIAAyEFAATowAsdAAMHaldl5LaljbP5BmVO8EE-KSG-qNsAArEXaxtceLlKCeEEQXVHUFoBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_фиолетовый_2": "AgACAgIAAyEFAATowAsdAAMIaldl6ltDh8YEYYq0isbfUkcTu1IAArIXaxtceLlKwPvuwk0I8YwBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_Graffiti_фиолетовый_3": "AgACAgIAAyEFAATowAsdAAMJaldl843bokGr7og4L3PZSxk-ECgAArMXaxtceLlKyXv4bGxlpngBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_белый_1": "AgACAgIAAyEFAATowAsdAANbalePAAHUmr9PROVmsjAtEOe6rINnAALnGGsbXHi5Sns75Y8M_lYOAQADAgADeQADPQQ",
+    "Bauer_Pulse_белый_2": "AgACAgIAAyEFAATowAsdAANcalePBfsYlU7vnrn6KiPy9Wd3MC0AAugYaxtceLlKU4weWcsJLkQBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_белый_3": "AgACAgIAAyEFAATowAsdAANdalePGJalxZXL3EFb2llk5bjbYGwAAukYaxtceLlK5e26diSzC3wBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_бирюзовый_1": "AgACAgIAAyEFAATowAsdAAMEaldlL91-xvMkotq093vrOCqpdEUAAqcXaxtceLlKiP1wxNkPu_oBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_бирюзовый_2": "AgACAgIAAyEFAATowAsdAAMFaldlNQISx1r0o3VSp0t8nWerp2cAAqgXaxtceLlKiAzmCA1IJ0IBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_бирюзовый_3": "AgACAgIAAyEFAATowAsdAAMGaldlPT3d8ngFE9TpKi6H7vOpOJcAAqkXaxtceLlKe3yq2YFx8JIBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_золотой_1": "AgACAgIAAyEFAATowAsdAANealePJ5sx3r9_soeXb5wAAWGZRfscAALrGGsbXHi5SvrecMiDCBEbAQADAgADeQADPQQ",
+    "Bauer_Pulse_золотой_2": "AgACAgIAAyEFAATowAsdAANfalePLeQIAedCAoQtwDhmBDgY6ukAAuwYaxtceLlKKBzhqluzPYsBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_золотой_3": "AgACAgIAAyEFAATowAsdAANaaleO70KkgPXJKL0vCAABYTNW76S3AALmGGsbXHi5ShFd0XytnVW_AQADAgADeQADPQQ",
+    "Bauer_Pulse_красный_1": "AgACAgIAAyEFAATowAsdAANYaleO2j3_H8KZssveDpwYD47-SK8AAuMYaxtceLlKRBXTNq-BfeUBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_красный_2": "AgACAgIAAyEFAATowAsdAANZaleO3saOgL1_y8i_ys5pSSjmgNkAAuQYaxtceLlKZB8cGYl8x-sBAAMCAAN5AAM9BA",
+    "Bauer_Pulse_красный_3": "AgACAgIAAyEFAATowAsdAANaaleO70KkgPXJKL0vCAABYTNW76S3AALmGGsbXHi5ShFd0XytnVW_AQADAgADeQADPQQ",
+    "Bauer_Tracer_красный_1": "AgACAgIAAyEFAATowAsdAAMbaldn7XqOzFZQ7J152qegFMm-YqAAAtEXaxtceLlKVyHhH1lb-pcBAAMCAAN4AAM9BA",
+    "Bauer_Tracer_красный_2": "AgACAgIAAyEFAATowAsdAAMcaldn-CVWxf13b7AoigkFw2AW7AEAAtIXaxtceLlKC_hp_atseIEBAAMCAAN5AAM9BA",
+    "Bauer_Tracer_красный_3": "AgACAgIAAyEFAATowAsdAAMdaldn_0FoQ0isrpf8iUBmuUH01cwAAtMXaxtceLlKqxq5c7BMkQ8BAAMCAAN5AAM9BA",
+    "Bauer_Tracer_синий_1": "AgACAgIAAyEFAATowAsdAAMYaldnwOmOI8PiAAGNp9cQbmC7941GAALMF2sbXHi5SgfYpbRyJbmYAQADAgADeQADPQQ",
+    "Bauer_Tracer_синий_2": "AgACAgIAAyEFAATowAsdAAMZaldnx9fzfFyZBWIL11Ir-KXuBmYAAs0XaxtceLlKVWgEdaEWx8gBAAMCAAN5AAM9BA",
+    "Bauer_Tracer_синий_3": "AgACAgIAAyEFAATowAsdAAMaaldnzpoHnfoFhxyX4VyfsUmSBXgAAs8XaxtceLlK5Nmz_Fm3RvgBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_зеленый_1": "AgACAgIAAyEFAATowAsdAAMKaldmRAT2fFgnBM8BmQGdASHrPvkAArYXaxtceLlKNfObfp8nfOwBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_зеленый_2": "AgACAgIAAyEFAATowAsdAAMLaldmTCGZbNJ6DeVkdI9QBspqkQgAArcXaxtceLlKOuF8pX43tEYBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_зеленый_3": "AgACAgIAAyEFAATowAsdAAMMaldmWOKFw2Bl_p4cjgnFnPHbAhUAArgXaxtceLlK44BHQbfhVWQBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_красный_1": "AgACAgIAAyEFAATowAsdAAMPaldmfpk69PzCUlp6J5vtARbysXMAArwXaxtceLlKrFnc20f-1DcBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_розовый_1": "AgACAgIAAyEFAATowAsdAAMRaldmlxxLGaodL-grsiC_cvCDu3kAAr4XaxtceLlKiA54QhGNMh8BAAMCAAN5AAM9BA",
+    "Bauer_Twitch_розовый_2": "AgACAgIAAyEFAATowAsdAANnalePktbvf7Y0W5SKFIwG2I27LDUAAvQYaxtceLlKNn61q9KWRV0BAAMCAAN5AAM9BA",
+    "Bauer_Twitch_розовый_3": "AgACAgIAAyEFAATowAsdAANoalePmE7BcC6U6i1CeMlBMxijVqEAAvgYaxtceLlKlmg5qPuSyxIBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_синий_1": "AgACAgIAAyEFAATowAsdAAMQaldmiilCxFdyw8ovcDEUkglrrScAAr0XaxtceLlKizpNPIO79uMBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_фиолетовый_1": "AgACAgIAAyEFAATowAsdAAMNaldmaRvZvjZIC5HcKF47kJtfJI0AAroXaxtceLlKTX3RGMYa8BIBAAMCAAN5AAM9BA",
+    "Bauer_Twitch_фиолетовый_2": "AgACAgIAAyEFAATowAsdAAMOaldmcLFLKkF9GOJy2x0C3bRYZxUAArsXaxtceLlKfJptcAABKn2IAQADAgADeQADPQQ",
+    "CCM_FT9_PRO_голубой_1": "AgACAgIAAyEFAATowAsdAAMqaldtXd5P5ncXkd9LXX3ZLfo2p7AAAvUXaxtceLlKgYOsiYJpxygBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_голубой_2": "AgACAgIAAyEFAATowAsdAAMraldtZlc0QH_cT9IBKB7_OuiGMxsAAvcXaxtceLlKEwZj4nGxCIsBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_голубой_3": "AgACAgIAAyEFAATowAsdAAMsaldtatgM-WDkrGoe8lwBrZdULJcAAvgXaxtceLlKf1B8ze1aGsoBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_желтый_1": "AgACAgIAAyEFAATowAsdAAMtaldtpen7imEYQgY0ona_HWQQn-8AAvkXaxtceLlKt7jWOZjoPpIBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_желтый_2": "AgACAgIAAyEFAATowAsdAAMualdtvTw8ixyhDSMO2gL3jCYHi0IAAvoXaxtceLlKctVKxHZRjZYBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_желтый_3": "AgACAgIAAyEFAATowAsdAAMvaldtvQI4xNhiAu9nv-u8hqTZVXcAAvsXaxtceLlKLpT0FVreI1oBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_красный_1": "AgACAgIAAyEFAATowAsdAAMualdtvTw8ixyhDSMO2gL3jCYHi0IAAvoXaxtceLlKctVKxHZRjZYBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_красный_2": "AgACAgIAAyEFAATowAsdAAMtaldtpen7imEYQgY0ona_HWQQn-8AAvkXaxtceLlKt7jWOZjoPpIBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_красный_3": "AgACAgIAAyEFAATowAsdAAMvaldtvQI4xNhiAu9nv-u8hqTZVXcAAvsXaxtceLlKLpT0FVreI1oBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_розовый_1": "AgACAgIAAyEFAATowAsdAAM2alduCIDqgn71Q1YyTgGiKvlUcOUAAv4XaxtceLlK5PVuFt9GoCgBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_розовый_2": "AgACAgIAAyEFAATowAsdAAMualdtvTw8ixyhDSMO2gL3jCYHi0IAAvoXaxtceLlKctVKxHZRjZYBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_розовый_3": "AgACAgIAAyEFAATowAsdAAMtaldtpen7imEYQgY0ona_HWQQn-8AAvkXaxtceLlKt7jWOZjoPpIBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_серый_1": "AgACAgIAAyEFAATowAsdAAM3alduKk7wf0V9RPA4bRwOI2hZ2tEAAv8XaxtceLlKx58nf_-06l8BAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_серый_2": "AgACAgIAAyEFAATowAsdAANsaleP3_9Kxi-bJZGLzdfS3wNC1KcAAv8YaxtceLlKxR79knUsJkwBAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_синий_1": "AgACAgIAAyEFAATowAsdAANqaleP1rLvaD6gdVoBypjfzLwCyAsAAv0YaxtceLlK98-lvKKDWnABAAMCAAN5AAM9BA",
+    "CCM_FT9_PRO_синий_2": "AgACAgIAAyEFAATowAsdAANraleP2iDNzXKGAAGrlzpaof5kQ7rOAAL-GGsbXHi5Sm8nPTQXNw7kAQADAgADeQADPQQ",
+    "CCM_FT9_PRO_синий_3": "AgACAgIAAyEFAATowAsdAANsaleP3_9Kxi-bJZGLzdfS3wNC1KcAAv8YaxtceLlKxR79knUsJkwBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_белый_1": "AgACAgIAAyEFAATowAsdAAM4alduVzKBaCV6CSviPdn8W1FRsZgAAgQYaxtceLlK7mG8nB97o_QBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_белый_2": "AgACAgIAAyEFAATowAsdAAM5alduXIRIgZ6RQAjtgGA8dOkD9SYAAgUYaxtceLlKu9vVKXitA7QBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_белый_3": "AgACAgIAAyEFAATowAsdAAM6alduYnn2Cw-c3UmQg5ThOqM18MAAAgYYaxtceLlK_snRQCy0siQBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_серый_1": "AgACAgIAAyEFAATowAsdAANualeQAZubZUgt8DdGUMNjcIWXlzQAAgEZaxtceLlKzz80fjjYA8YBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_серый_2": "AgACAgIAAyEFAATowAsdAANvaleQBcp5uq2SW7TSi8FAMMuvSfYAAgIZaxtceLlK7Ln42-HZOwQBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_серый_3": "AgACAgIAAyEFAATowAsdAANwaleQCaHASu4oFVh-ebs8dMd5TKkAAgQZaxtceLlKXOD-0VdBqncBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_фиолетовый_1": "AgACAgIAAyEFAATowAsdAAMValdnRb2KTiwHBILeI4yeTdCfHRwAAsgXaxtceLlKjf7AvfBwAikBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_фиолетовый_2": "AgACAgIAAyEFAATowAsdAAMWaldnTUNgQVqj1cqQexePIWc133sAAskXaxtceLlK6tCRifYy4TEBAAMCAAN5AAM9BA",
+    "CCM_Trigger_10_PRO_фиолетовый_3": "AgACAgIAAyEFAATowAsdAAMXaldnk4BPJklXELBGOcqd4FfNkWIAAsoXaxtceLlKCVQ6R5Z8NrwBAAMCAAN5AAM9BA",
+    "CCM_Trigger_Unleashed_белый_1": "AgACAgIAAyEFAATowAsdAANKaldvzmhq1eftbcG9HzDsyRZ5JjsAAicYaxtceLlKwamEDdlQlEYBAAMCAAN5AAM9BA",
+    "CCM_Trigger_Unleashed_белый_2": "AgACAgIAAyEFAATowAsdAANLaldv2aSrrXs9FgyNijdx3yJ5qqgAAigYaxtceLlKvK-fZdCe3psBAAMCAAN5AAM9BA",
+    "CCM_Trigger_Unleashed_белый_3": "AgACAgIAAyEFAATowAsdAANMaldv5GgzVFPDLNr-e98ljBImP3kAAikYaxtceLlKOsXq5M-IBVgBAAMCAAN5AAM9BA",
+    "CCM_Trigger_Unleashed_черный_1": "AgACAgIAAyEFAATowAsdAANIaldvrZPmPE2cGXnbCgjV2JwXlZkAAiUYaxtceLlKUQiHFovl8p4BAAMCAAN5AAM9BA",
+    "CCM_Trigger_Unleashed_черный_2": "AgACAgIAAyEFAATowAsdAANJaldvuAoQNOJiFx2K_elPlRv12eMAAiYYaxtceLlKH0UuRtnbqcEBAAMCAAN5AAM9BA",
+    "CCM_Trigger_Unleashed_черный_3": "AgACAgIAAyEFAATowAsdAANpalePrkl0DjJtUY0QVrISzw76sCoAAvoYaxtceLlKS-NAfUHiw4cBAAMCAAN5AAM9BA",
+    "CCM_Vision_стандартный_1": "AgACAgIAAyEFAATowAsdAAMSaldm2CxUbAPIkjKU0B5I34QCcxUAAsAXaxtceLlKqUJvdulZwUgBAAMCAAN5AAM9BA",
+    "CCM_Vision_стандартный_2": "AgACAgIAAyEFAATowAsdAAMTaldnAnlBsAGRkmXWb6xC-i4KjSAAAsIXaxtceLlKOQumMWUaHB0BAAMCAAN5AAM9BA",
+    "CCM_Vision_стандартный_3": "AgACAgIAAyEFAATowAsdAAMUaldnCMo-GekeWyJWTyR4RjKGLh8AAsMXaxtceLlKIdlqHqDjv48BAAMCAAN4AAM9BA",
+    "CCM_XF_Ghost_белый_1": "AgACAgIAAyEFAATowAsdAANEaldvVpB5Dfl2f0TKPNfsc5CC7boAAh0YaxtceLlKBwHvI2Jn2lkBAAMCAAN5AAM9BA",
+    "CCM_XF_Ghost_белый_2": "AgACAgIAAyEFAATowAsdAANFaldvXC9joSEkKqaGnXHFwX9GTkUAAiEYaxtceLlK4OCNCnVUFXMBAAMCAAN5AAM9BA",
+    "CCM_XF_Ghost_белый_3": "AgACAgIAAyEFAATowAsdAANGaldvYa6fpJhWSEB5151bRWcmotgAAiIYaxtceLlK0RWY7cOC0g0BAAMCAAN5AAM9BA",
+    "CCM_XF_Ghost_черный_1": "AgACAgIAAyEFAATowAsdAAM7aldug42b7yuhOGdY2dLQW_4EMhEAAggYaxtceLlKyAwKId8Ic30BAAMCAAN5AAM9BA",
+    "CCM_XF_Ghost_черный_2": "AgACAgIAAyEFAATowAsdAAM-alduv6KTUzf75pwryQ9q6pGRtDYAAg0YaxtceLlKLp1fX8G17HIBAAMCAAN5AAM9BA",
+    "CCM_XF_Ghost_черный_3": "AgACAgIAAyEFAATowAsdAAM_alduyt4DxzPdsckax8QmWpeRoXIAAg8YaxtceLlKrb8YAAHu2ViCAQADAgADeQADPQQ",
+}
+
+
+def get_photos(model: str, color: str = None) -> list:
+    """Возвращает список file_id фото для модели и цвета."""
+    model_key = model.strip().replace(" ", "_")
+    if color:
+        color_key = color.strip().replace(" ", "_")
+        prefix = model_key + "_" + color_key
+    else:
+        prefix = model_key
+
+    result = []
+    for i in range(1, 10):
+        key = f"{prefix}_{i}"
+        if key in PHOTOS:
+            result.append(PHOTOS[key])
+
+    # Если по цвету не нашли - ищем любые фото этой модели
+    if not result:
+        for key, fid in PHOTOS.items():
+            if key.startswith(model_key + "_"):
+                result.append(fid)
+                if len(result) >= 3:
+                    break
+
+    return result
+
+
+def get_videos(model: str, color: str = None) -> list:
+    """Возвращает список file_id видео для модели и цвета. Пока пустой."""
+    return []
